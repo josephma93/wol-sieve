e2e14085d6884331ddee17c8190068ca49087431
Joseph Montero <3136168+josephma93@users.noreply.github.com>
2024-12-10 14:51:38 -0600

    add simple index to simplify navigation

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,35 @@ app.use(pinoHttp({ logger: logger.child({ ...logger.bindings(), label: 'wol' }) 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false, limit: '512kb' }));
 
+app.get('/', (_, res) => {
+	res.send(`<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<ul>
+		<li><a href="wol/landing-html">GET wol/landing-html</a></li>
+		<li><a href="wol/mid-week-program-html">GET wol/mid-week-program-html</a></li>
+		<li><a href="/pub-mwb/">GET & POST /pub-mwb/</a></li>
+		<li><a href="/pub-mwb/scrappers/week-date-span">GET & POST /pub-mwb/scrappers/week-date-span</a></li>
+		<li><a href="/pub-mwb/scrappers/songs">GET & POST /pub-mwb/scrappers/songs</a></li>
+		<li><a href="/pub-mwb/scrappers/weekly-bible-read">GET & POST /pub-mwb/scrappers/weekly-bible-read</a></li>
+		<li><a href="/pub-mwb/scrappers/treasures-talk">GET & POST /pub-mwb/scrappers/treasures-talk</a></li>
+		<li><a href="/pub-mwb/scrappers/spiritual-gems">GET & POST /pub-mwb/scrappers/spiritual-gems</a></li>
+		<li><a href="/pub-mwb/scrappers/bible-read-details">GET & POST /pub-mwb/scrappers/bible-read-details</a></li>
+		<li><a href="/pub-mwb/scrappers/field-ministry">GET & POST /pub-mwb/scrappers/field-ministry</a></li>
+		<li><a href="/pub-mwb/scrappers/christian-living">GET & POST /pub-mwb/scrappers/christian-living</a></li>
+		<li><a href="/pub-mwb/scrappers/bible-study">GET & POST /pub-mwb/scrappers/bible-study</a></li>
+		<li><a href="/pub-w/">GET /pub-w/</a></li>
+		<li><a href="/pub-w/html">GET & POST /pub-w/html</a></li>
+		<li><a href="/pub-nwtsty/">GET & POST /pub-w/html</a></li>
+	</ul>
+</body>
+</html>`);
+});
 app.use('/wol', wolRouter);
 app.use('/pub-mwb', pubMwbRouter);
 app.use('/pub-w', pubWRouter);
